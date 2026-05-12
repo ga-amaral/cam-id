f74fcdc710683276ae6d0fa46dab1bfc182af63c
ga-amaral <amaralgabriel123@gmail.com>
2026-05-11 23:46:50 -0300

    Fix API endpoint

--- api/identify.js
+++ api/identify.js
@@ -1,5 +1,3 @@
-const { createClient } = require('@vercel/kv');
-
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
